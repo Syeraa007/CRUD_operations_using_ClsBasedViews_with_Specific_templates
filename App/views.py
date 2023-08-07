9fb2358f9d844e6c3bb3591f58b5ab165ad5b29e
SYERAA007 <simhasimha4321.com>
2023-08-07 16:01:17 +0530

    first commit

--- App/views.py
+++ App/views.py
@@ -6,14 +6,16 @@ from App.models import *
 
 class SchoolList(ListView):
     model=School
-    template_name='App/school_list.html'
+    # template_name='App/school_list.html'
+    # templates will be directly rendered by ListView implicitly
     context_object_name='ASO'
     # queryset=School.objects.all()
     ordering=['id']
 
 class StudentList(ListView):
     model=Student
-    template_name='App/student_list.html'
+    # template_name='App/student_list.html'
+    # templates will be directly rendered by ListView implicitly
     context_object_name='ASO'
     # queryset=Student.objects.all()
     ordering=['id']
